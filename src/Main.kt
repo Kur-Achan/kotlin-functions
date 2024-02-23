af1f5224ec39889737453056a65a0b40cc41609c
Kur-Achan <you@example.com>
2024-02-23 08:49:22 +0300

    add file

---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+import javax.naming.Name
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+  println("Hello Achan")
+    SumOfNumbers(4,20,17,23)
+    modulus(16,32)
+    println("I love swimming and travelling")
+    }
+fun printMyName(name: String){
+    println(name)
+}
+fun SumOfNumbers(num1:Int,num2:Int, num3: Int ,num4:Int):Int{
+    var result = num1+num2+num3+num4
+    return result
+
+}
+fun modulus(num1:Int,num2: Int):Int{
+    var result = num1 % num2
+    return  result
+}
+fun  printMyInterests(name: String){
+    println(name)
+}
